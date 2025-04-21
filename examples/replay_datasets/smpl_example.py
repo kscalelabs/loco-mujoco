c3c641c9d18e9c86644e8420b5d9dd147a002fdc
alik-git <ali@kscale.dev>
2025-04-20 20:44:07 -0700

    update example script

--- examples/replay_datasets/smpl_example.py
+++ examples/replay_datasets/smpl_example.py
@@ -1,5 +1,10 @@
 import numpy as np
-from loco_mujoco.task_factories import ImitationFactory, LAFAN1DatasetConf, DefaultDatasetConf, AMASSDatasetConf
+from loco_mujoco.task_factories import (
+    ImitationFactory,
+    LAFAN1DatasetConf,
+    DefaultDatasetConf,
+    AMASSDatasetConf,
+)
 
 
 def experiment(seed=0):
@@ -7,14 +12,24 @@ def experiment(seed=0):
     np.random.seed(seed)
 
     # # example --> you can add as many datasets as you want in the lists!
-    env = ImitationFactory.make("UnitreeG1",
-                                # if SMPL and AMASS are installed, you can use the following:
-                                amass_dataset_conf=AMASSDatasetConf(["DanceDB/DanceDB/20120911_TheodorosSourmelis/Capoeira_Theodoros_v2_C3D_poses"]),
-                                n_substeps=20)
+    env = ImitationFactory.make(
+        # "UnitreeG1",
+        "KBotV2",
+        # if SMPL and AMASS are installed, you can use the following:
+        amass_dataset_conf=AMASSDatasetConf(
+            [
+                "ACCAD/ACCAD/Male1Walking_c3d/Walk B13 - Walk turn right 45_poses",
+                "ACCAD/ACCAD/Male2Running_c3d/C1 - stand to run_poses",
+                "ACCAD/ACCAD/s001/EricCamper04_poses.npz",
+            ]
+        ),
+        # amass_dataset_conf=AMASSDatasetConf(["DanceDB/DanceDB/20120911_TheodorosSourmelis/Capoeira_Theodoros_v2_C3D_poses"]),
+        n_substeps=20,
+    )
 
     env.play_trajectory(n_episodes=3, n_steps_per_episode=500, render=True)
-    # env.play_trajectory(n_episodes=3, n_steps_per_episode=500, render=True, record=True)
+    # env.play_trajectory(n_episodes=1, n_steps_per_episode=500, render=True, record=True)
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     experiment()
