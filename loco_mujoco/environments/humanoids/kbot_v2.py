bbf81331c009530b79e991e6d1c13c7e3942590b
alik-git <ali@kscale.dev>
2025-04-20 21:16:13 -0700

    format

--- loco_mujoco/environments/humanoids/kbot_v2.py
+++ loco_mujoco/environments/humanoids/kbot_v2.py
@@ -14,13 +14,15 @@ class KBotV2(BaseRobotHumanoid):
     Mujoco environment of the KBotV2 robot.
     """
 
-    mjx_enabled = False # Set to True if you create a Mjx-compatible version
+    mjx_enabled = False  # Set to True if you create a Mjx-compatible version
 
-    def __init__(self,
-                 spec: Union[str, MjSpec] = None,
-                 observation_spec: List[Observation] = None,
-                 actuation_spec: List[str] = None,
-                 **kwargs) -> None:
+    def __init__(
+        self,
+        spec: Union[str, MjSpec] = None,
+        observation_spec: List[Observation] = None,
+        actuation_spec: List[str] = None,
+        **kwargs,
+    ) -> None:
         """
         Constructor.
 
@@ -51,7 +53,12 @@ class KBotV2(BaseRobotHumanoid):
         # modify the specification if needed (add logic here if needed for variations)
         # e.g., if self.mjx_enabled: spec = self._modify_spec_for_mjx(spec)
 
-        super().__init__(spec=spec, actuation_spec=actuation_spec, observation_spec=observation_spec, **kwargs)
+        super().__init__(
+            spec=spec,
+            actuation_spec=actuation_spec,
+            observation_spec=observation_spec,
+            **kwargs,
+        )
 
     @staticmethod
     def _get_observation_specification(spec: MjSpec) -> List[Observation]:
@@ -65,62 +72,104 @@ class KBotV2(BaseRobotHumanoid):
             List[Observation]: List of observations.
         """
         # Adapt joint names based on your kbot_v2.xml
-        observation_spec = [# ------------- JOINT POS -------------
-                            ObservationType.FreeJointPosNoXY("q_root", xml_name="root"),
-                            ObservationType.JointPos("q_right_shoulder_pitch", xml_name="dof_right_shoulder_pitch_03"),
-                            ObservationType.JointPos("q_right_shoulder_roll", xml_name="dof_right_shoulder_roll_03"),
-                            ObservationType.JointPos("q_right_shoulder_yaw", xml_name="dof_right_shoulder_yaw_02"),
-                            ObservationType.JointPos("q_right_elbow", xml_name="dof_right_elbow_02"),
-                            ObservationType.JointPos("q_right_wrist", xml_name="dof_right_wrist_00"),
-                            ObservationType.JointPos("q_left_shoulder_pitch", xml_name="dof_left_shoulder_pitch_03"),
-                            ObservationType.JointPos("q_left_shoulder_roll", xml_name="dof_left_shoulder_roll_03"),
-                            ObservationType.JointPos("q_left_shoulder_yaw", xml_name="dof_left_shoulder_yaw_02"),
-                            ObservationType.JointPos("q_left_elbow", xml_name="dof_left_elbow_02"),
-                            ObservationType.JointPos("q_left_wrist", xml_name="dof_left_wrist_00"),
-                            ObservationType.JointPos("q_right_hip_pitch", xml_name="dof_right_hip_pitch_04"),
-                            ObservationType.JointPos("q_right_hip_roll", xml_name="dof_right_hip_roll_03"),
-                            ObservationType.JointPos("q_right_hip_yaw", xml_name="dof_right_hip_yaw_03"),
-                            ObservationType.JointPos("q_right_knee", xml_name="dof_right_knee_04"),
-                            ObservationType.JointPos("q_right_ankle", xml_name="dof_right_ankle_02"),
-                            ObservationType.JointPos("q_left_hip_pitch", xml_name="dof_left_hip_pitch_04"),
-                            ObservationType.JointPos("q_left_hip_roll", xml_name="dof_left_hip_roll_03"),
-                            ObservationType.JointPos("q_left_hip_yaw", xml_name="dof_left_hip_yaw_03"),
-                            ObservationType.JointPos("q_left_knee", xml_name="dof_left_knee_04"),
-                            ObservationType.JointPos("q_left_ankle", xml_name="dof_left_ankle_02"),
-
-                            # ------------- JOINT VEL -------------
-                            ObservationType.FreeJointVel("dq_root", xml_name="root"),
-                            ObservationType.JointVel("dq_right_shoulder_pitch", xml_name="dof_right_shoulder_pitch_03"),
-                            ObservationType.JointVel("dq_right_shoulder_roll", xml_name="dof_right_shoulder_roll_03"),
-                            ObservationType.JointVel("dq_right_shoulder_yaw", xml_name="dof_right_shoulder_yaw_02"),
-                            ObservationType.JointVel("dq_right_elbow", xml_name="dof_right_elbow_02"),
-                            ObservationType.JointVel("dq_right_wrist", xml_name="dof_right_wrist_00"),
-                            ObservationType.JointVel("dq_left_shoulder_pitch", xml_name="dof_left_shoulder_pitch_03"),
-                            ObservationType.JointVel("dq_left_shoulder_roll", xml_name="dof_left_shoulder_roll_03"),
-                            ObservationType.JointVel("dq_left_shoulder_yaw", xml_name="dof_left_shoulder_yaw_02"),
-                            ObservationType.JointVel("dq_left_elbow", xml_name="dof_left_elbow_02"),
-                            ObservationType.JointVel("dq_left_wrist", xml_name="dof_left_wrist_00"),
-                            ObservationType.JointVel("dq_right_hip_pitch", xml_name="dof_right_hip_pitch_04"),
-                            ObservationType.JointVel("dq_right_hip_roll", xml_name="dof_right_hip_roll_03"),
-                            ObservationType.JointVel("dq_right_hip_yaw", xml_name="dof_right_hip_yaw_03"),
-                            ObservationType.JointVel("dq_right_knee", xml_name="dof_right_knee_04"),
-                            ObservationType.JointVel("dq_right_ankle", xml_name="dof_right_ankle_02"),
-                            ObservationType.JointVel("dq_left_hip_pitch", xml_name="dof_left_hip_pitch_04"),
-                            ObservationType.JointVel("dq_left_hip_roll", xml_name="dof_left_hip_roll_03"),
-                            ObservationType.JointVel("dq_left_hip_yaw", xml_name="dof_left_hip_yaw_03"),
-                            ObservationType.JointVel("dq_left_knee", xml_name="dof_left_knee_04"),
-                            ObservationType.JointVel("dq_left_ankle", xml_name="dof_left_ankle_02"),
-
-                            # ------------- SENSORS -------------
-                            # ObservationType.SensorAccelerometer("imu_acc", xml_name="imu_acc"),
-                            # ObservationType.SensorGyro("imu_gyro", xml_name="imu_gyro"),
-                            # ObservationType.SensorMagnetometer("imu_mag", xml_name="imu_mag"),
-                            # ObservationType.SensorFramePos("base_link_pos", xml_name="base_link_pos"),
-                            # ObservationType.SensorFrameQuat("base_link_quat", xml_name="base_link_quat"),
-                            # ObservationType.SensorFrameLinVel("base_link_vel", xml_name="base_link_vel"),
-                            # ObservationType.SensorFrameAngVel("base_link_ang_vel", xml_name="base_link_ang_vel"),
-                            # ObservationType.SensorForce("left_foot_force", xml_name="left_foot_force"),
-                            # ObservationType.SensorForce("right_foot_force", xml_name="right_foot_force")
+        observation_spec = [  # ------------- JOINT POS -------------
+            ObservationType.FreeJointPosNoXY("q_root", xml_name="root"),
+            ObservationType.JointPos(
+                "q_right_shoulder_pitch", xml_name="dof_right_shoulder_pitch_03"
+            ),
+            ObservationType.JointPos(
+                "q_right_shoulder_roll", xml_name="dof_right_shoulder_roll_03"
+            ),
+            ObservationType.JointPos(
+                "q_right_shoulder_yaw", xml_name="dof_right_shoulder_yaw_02"
+            ),
+            ObservationType.JointPos("q_right_elbow", xml_name="dof_right_elbow_02"),
+            ObservationType.JointPos("q_right_wrist", xml_name="dof_right_wrist_00"),
+            ObservationType.JointPos(
+                "q_left_shoulder_pitch", xml_name="dof_left_shoulder_pitch_03"
+            ),
+            ObservationType.JointPos(
+                "q_left_shoulder_roll", xml_name="dof_left_shoulder_roll_03"
+            ),
+            ObservationType.JointPos(
+                "q_left_shoulder_yaw", xml_name="dof_left_shoulder_yaw_02"
+            ),
+            ObservationType.JointPos("q_left_elbow", xml_name="dof_left_elbow_02"),
+            ObservationType.JointPos("q_left_wrist", xml_name="dof_left_wrist_00"),
+            ObservationType.JointPos(
+                "q_right_hip_pitch", xml_name="dof_right_hip_pitch_04"
+            ),
+            ObservationType.JointPos(
+                "q_right_hip_roll", xml_name="dof_right_hip_roll_03"
+            ),
+            ObservationType.JointPos(
+                "q_right_hip_yaw", xml_name="dof_right_hip_yaw_03"
+            ),
+            ObservationType.JointPos("q_right_knee", xml_name="dof_right_knee_04"),
+            ObservationType.JointPos("q_right_ankle", xml_name="dof_right_ankle_02"),
+            ObservationType.JointPos(
+                "q_left_hip_pitch", xml_name="dof_left_hip_pitch_04"
+            ),
+            ObservationType.JointPos(
+                "q_left_hip_roll", xml_name="dof_left_hip_roll_03"
+            ),
+            ObservationType.JointPos("q_left_hip_yaw", xml_name="dof_left_hip_yaw_03"),
+            ObservationType.JointPos("q_left_knee", xml_name="dof_left_knee_04"),
+            ObservationType.JointPos("q_left_ankle", xml_name="dof_left_ankle_02"),
+            # ------------- JOINT VEL -------------
+            ObservationType.FreeJointVel("dq_root", xml_name="root"),
+            ObservationType.JointVel(
+                "dq_right_shoulder_pitch", xml_name="dof_right_shoulder_pitch_03"
+            ),
+            ObservationType.JointVel(
+                "dq_right_shoulder_roll", xml_name="dof_right_shoulder_roll_03"
+            ),
+            ObservationType.JointVel(
+                "dq_right_shoulder_yaw", xml_name="dof_right_shoulder_yaw_02"
+            ),
+            ObservationType.JointVel("dq_right_elbow", xml_name="dof_right_elbow_02"),
+            ObservationType.JointVel("dq_right_wrist", xml_name="dof_right_wrist_00"),
+            ObservationType.JointVel(
+                "dq_left_shoulder_pitch", xml_name="dof_left_shoulder_pitch_03"
+            ),
+            ObservationType.JointVel(
+                "dq_left_shoulder_roll", xml_name="dof_left_shoulder_roll_03"
+            ),
+            ObservationType.JointVel(
+                "dq_left_shoulder_yaw", xml_name="dof_left_shoulder_yaw_02"
+            ),
+            ObservationType.JointVel("dq_left_elbow", xml_name="dof_left_elbow_02"),
+            ObservationType.JointVel("dq_left_wrist", xml_name="dof_left_wrist_00"),
+            ObservationType.JointVel(
+                "dq_right_hip_pitch", xml_name="dof_right_hip_pitch_04"
+            ),
+            ObservationType.JointVel(
+                "dq_right_hip_roll", xml_name="dof_right_hip_roll_03"
+            ),
+            ObservationType.JointVel(
+                "dq_right_hip_yaw", xml_name="dof_right_hip_yaw_03"
+            ),
+            ObservationType.JointVel("dq_right_knee", xml_name="dof_right_knee_04"),
+            ObservationType.JointVel("dq_right_ankle", xml_name="dof_right_ankle_02"),
+            ObservationType.JointVel(
+                "dq_left_hip_pitch", xml_name="dof_left_hip_pitch_04"
+            ),
+            ObservationType.JointVel(
+                "dq_left_hip_roll", xml_name="dof_left_hip_roll_03"
+            ),
+            ObservationType.JointVel("dq_left_hip_yaw", xml_name="dof_left_hip_yaw_03"),
+            ObservationType.JointVel("dq_left_knee", xml_name="dof_left_knee_04"),
+            ObservationType.JointVel("dq_left_ankle", xml_name="dof_left_ankle_02"),
+            # ------------- SENSORS -------------
+            # ObservationType.SensorAccelerometer("imu_acc", xml_name="imu_acc"),
+            # ObservationType.SensorGyro("imu_gyro", xml_name="imu_gyro"),
+            # ObservationType.SensorMagnetometer("imu_mag", xml_name="imu_mag"),
+            # ObservationType.SensorFramePos("base_link_pos", xml_name="base_link_pos"),
+            # ObservationType.SensorFrameQuat("base_link_quat", xml_name="base_link_quat"),
+            # ObservationType.SensorFrameLinVel("base_link_vel", xml_name="base_link_vel"),
+            # ObservationType.SensorFrameAngVel("base_link_ang_vel", xml_name="base_link_ang_vel"),
+            # ObservationType.SensorForce("left_foot_force", xml_name="left_foot_force"),
+            # ObservationType.SensorForce("right_foot_force", xml_name="right_foot_force")
         ]
 
         return observation_spec
@@ -173,11 +222,25 @@ class KBotV2(BaseRobotHumanoid):
         """
         Returns the default sites that are used for mimic. Needs to match the sites added in kbot_v2.xml.
         """
-        return ["pelvis_mimic", "upper_body_mimic", "head_mimic",
-                "left_shoulder_mimic", "left_elbow_mimic", "left_wrist_mimic",
-                "left_hip_mimic", "left_knee_mimic", "left_ankle_mimic", "left_foot",
-                "right_shoulder_mimic", "right_elbow_mimic", "right_wrist_mimic",
-                "right_hip_mimic", "right_knee_mimic", "right_ankle_mimic", "right_foot"]
+        return [
+            "pelvis_mimic",
+            "upper_body_mimic",
+            "head_mimic",
+            "left_shoulder_mimic",
+            "left_elbow_mimic",
+            "left_wrist_mimic",
+            "left_hip_mimic",
+            "left_knee_mimic",
+            "left_ankle_mimic",
+            "left_foot",
+            "right_shoulder_mimic",
+            "right_elbow_mimic",
+            "right_wrist_mimic",
+            "right_hip_mimic",
+            "right_knee_mimic",
+            "right_ankle_mimic",
+            "right_foot",
+        ]
 
     @info_property
     def root_body_name(self) -> str:
@@ -209,4 +272,4 @@ class KBotV2(BaseRobotHumanoid):
         Returns the healthy range of the root height. This is only used when HeightBasedTerminalStateHandler is used.
         Adjust these values based on your robot's expected operational height.
         """
-        return (0.7, 1.5) # Example range, adjust as needed 
+        return (0.7, 1.5)  # Example range, adjust as needed
